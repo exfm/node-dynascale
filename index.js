4074990fef72f5f881a4385748478b977afc3f02
Drew Harris <contac@grmnygrmny.com>
2012-12-07 12:31:27 -0500

    big changes. dumb scaling by 120% when reaching a threshold of failed requests.

--- index.js
+++ index.js
@@ -5,31 +5,48 @@ var when = require('when'),
 	aws = require('plata'),
 	AWS = require('aws-sdk'),
 	getConfig = require('junto'),
-	nconf = require('nconf'),
 	util = require('util'),
 	express = require('express');
 
 var app = express(),
 	config,
-	dynamo;
+	dynamo,
+	exceptionCounter = {},
+	checkExceptions,
+	tables,
+	threshold = 20,
+	interval = 60000,
+	scaleValue = 1.20;
 
-nconf.file({'file': 'config.json'});
+// setup
 
-getConfig('development').then(function(c){
-	config = c;
-	aws.connect(config.aws);
+sequence().then(function(next){
+	getConfig('development').then(function(c){
+		config = c;
+		aws.connect(config.aws);
+	});
+	aws.onConnected(next);
+}).then(function(next){
+	AWS.config.update({accessKeyId: config.aws.key, secretAccessKey: config.aws.secret});
+	AWS.config.update({region: 'us-east-1'});
+	dynamo = new AWS.DynamoDB();
+	listTables().then(next);
+}).then(function(next, tb){
+	tables = tb;
+	tables.map(function(tableName){
+		exceptionCounter[tableName] = {
+			'read': 0,
+			'write': 0
+		};
+	});
+	startCounter();
+	app.listen(8080);
+	console.log('Listening on 8080');
 });
 
-// checkThroughput();
-// setInterval(checkThroughput, 10000);
-
-// Run with a setInterval for every 5 minutes
-// Get CloudWatch data for the last 30 minutes
-// If levels for any metric have been about 70% for
-// more than 5 consecutive datapoints, trigger scaling
-
 app.get('/notification/:message', function(req, res){
-
+	// if this is a throughput exceeded exception,
+	// increment the relevant counter
 });
 
 app.get('/throughput', function(req, res){
@@ -38,8 +55,10 @@ app.get('/throughput', function(req, res){
 	});
 });
 
-app.listen(8080);
-console.log('Listening on 8080');
+
+// We're not really using this,
+// but I'm going to keep it
+// in here anyways.
 
 function checkThroughput(){
 	var d = when.defer(),
@@ -48,12 +67,7 @@ function checkThroughput(){
 		startTime = new Date(new Date().getTime() - 1000*60*timeRangeMinutes),
 		endTime = new Date();
 
-	aws.onConnected(function(){
-		AWS.config.update({accessKeyId: config.aws.key, secretAccessKey: config.aws.secret});
-		AWS.config.update({region: 'us-east-1'});
-		dynamo = new AWS.DynamoDB();
-
-		when.all(nconf.get('tables').map(function(table){
+		when.all(tables.map(function(table){
 			var p = when.defer();
 			dynamo.client.describeTable({
 				'TableName': table
@@ -65,7 +79,8 @@ function checkThroughput(){
 				};
 				getThroughputStatistics(tableName,
 					'write', startTime, endTime).then(function(writeStats){
-					if (writeStats.datapoints !== undefined){
+						console.log(writeStats);
+					if (writeStats.datapoints !== null){
 						writeStats.datapoints.map(function(pnt){
 							throughputStats[tableName].write.push({
 								'timestamp': pnt.timestamp,
@@ -75,7 +90,7 @@ function checkThroughput(){
 					}
 					getThroughputStatistics(res.data.Table.TableName,
 						'read', startTime, endTime).then(function(readStats){
-							if (readStats.datapoints !== undefined){
+							if (readStats.datapoints !== null){
 								readStats.datapoints.map(function(pnt){
 									throughputStats[tableName].read.push({
 										'timestamp': pnt.timestamp,
@@ -93,21 +108,6 @@ function checkThroughput(){
 		})).then(function(){
 			return d.resolve(throughputStats);
 		});
-
-	});
-	return d.promise;
-}
-
-function increaseThroughput(tableName, newThroughput){
-	var d = when.defer();
-	dynamo.client.updateTable({
-		'TableName': tableName,
-		'ProvisionedThroughput': newThroughput
-	}).done(function (res) {
-		return d.resolve();
-	}).fail(function(err){
-		return d.reject(new Error(err));
-	});
 	return d.promise;
 }
 
@@ -117,7 +117,6 @@ function getThroughputStatistics(tableName, action, startTime, endTime){
 			'write': 'ConsumedWriteCapacityUnits',
 			'read': 'ConsumedReadCapacityUnits'
 		};
-
 	aws.cloudWatch.getMetricStatistics('AWS/DynamoDB', metrics[action],
 		300, startTime.toISOString(), endTime.toISOString(),{
 			'Sum': '1'
@@ -127,4 +126,59 @@ function getThroughputStatistics(tableName, action, startTime, endTime){
 			d.resolve(data.getMetricStatisticsResponse.getMetricStatisticsResult);
 		});
 	return d.promise;
+}
+
+function scaleThroughput(tableName, action, scaleValue){
+	var d = when.defer(),
+		map = {
+			'write': 'WriteCapacityUnits',
+			'read': 'ReadCapacityUnits'
+		};
+	sequence().then(function(next){
+		dynamo.client.describeTable({
+			'TableName': tableName
+		}).done(function(res) {
+			next(res.data.Table.ProvisionedThroughput);
+		}).fail(function(err){
+			return d.reject(new Error(err));
+		});
+	}).then(function(next, throughput){
+		throughput[map[action]] *= scaleValue;
+		dynamo.client.updateTable({
+			'TableName': tableName,
+			'ProvisionedThroughput': throughput
+		}).done(function (res) {
+			return d.resolve();
+		}).fail(function(err){
+			return d.reject(new Error(err));
+		});
+	});
+	return d.promise;
+}
+
+function listTables(){
+	var d = when.defer();
+	dynamo.client.listTables().done(function(res){
+		d.resolve(res.data.TableNames);
+	}).fail(function(err){
+		d.reject(new Error(err));
+	});
+	return d.promise;
+}
+
+function startCounter(){
+	checkExceptions = setInterval(function(){
+		Object.keys(exceptionCounter).map(function(tableName){
+			var actions = ['read', 'write'];
+			actions.forEach(function(action){
+				if (exceptionCounter[tableName][action] >= threshold){
+					scaleThroughput(tableName, action, scaleValue).then(function(){
+						console.log('Successfully scaled table ' + tableName + 'by ' + scaleValue);
+					}, function(){
+						throw new Error('Unable to scale table ' + tableName);
+					});
+				}
+			});
+		});
+	}, interval);
 }
